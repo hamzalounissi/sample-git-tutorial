aa58f1a7b4ac3550bebd2d2c1e5f47e5fe2c34c4
hamzalounissi <hamzalounissi@gmail.com>
2020-04-20 18:46:38 +0000

    Add getter and setter for name field

--- eclipse-workspace/simple-git-projet/src/com/capgemini/model/User.java
+++ eclipse-workspace/simple-git-projet/src/com/capgemini/model/User.java
@@ -3,5 +3,15 @@ package com.capgemini.model;
 public class User {
 
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 	
 }
